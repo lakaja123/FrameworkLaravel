0916d620982671f164c7a16448220b837c83cf8d
lakaja123 <grealm@bk.ru>
2022-04-16 18:49:10 +0500

    version 1.0.9

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,60 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('my-name', function () {
+    return view('Максмм Дятлов');
+});
+
+Route::get('my-friend', function () {
+    return view('Николай Ананасов');
+});
+
+Route::get('get-friend/{name}', function ($name) {
+    return $name;
+});
+
+Route::get('my-city/{city}', function ($city) {
+    return $city;
+})->where('city', '[A-Z-a-z]+');
+
+Route::get('level/{lvl}', function ($lvl) {
+    if ($lvl > 0 && $lvl < 25)
+        return view('новичок');
+    if ($lvl > 25 && $lvl < 50)
+        return view('специалист');
+    if ($lvl > 50 && $lvl < 75)
+        return view('босс');
+    if ($lvl > 75 && $lvl < 99)
+        return view('старик');
+    if ($lvl == 99)
+        return view('король');
+});
+Route::get('working/{name}/{date}', function ($name, $date) {
+    return 'Name: ' .$name .' Date: ' .$date;
+});
+Route::get('power', function () {
+    return route('power');
+})->name('power');
+Route::prefix('admin')->group(function () {
+    Route::get('login', function () {
+        return route('login');
+    })->name('login');
+    Route::get('logout', function () {
+        return route('logout');
+    })->name('logout');
+    Route::get('info', function () {
+        return route('info');
+    })->name('info');
+    Route::get('color', function () {
+        return route('color');
+    })->name('color');
+});
+Route::get('admin/web', function () {
+    return view('admin/color');
+})->name('admin/web');
+Route::get('color/{hex}', function ($hex) {
+    //
+})->where('hex', '[A-F0-9]{1,6}');
+
+
